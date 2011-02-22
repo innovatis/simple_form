706391895e1a870ac75629a7e1bb87e9ffd95fc2
Burke Libbey <burke@burkelibbey.org>
2011-02-22 16:33:52 -0600

    bleh

--- lib/simple_form/inputs/dms_file_input.rb
+++ lib/simple_form/inputs/dms_file_input.rb
@@ -14,7 +14,19 @@ module SimpleForm
 
       def file_upload
         genid = (rand 2**32).to_s(16)
-        "<div class='file_upload' id='#{genid}'></div>".html_safe
+
+        scope = "#{object.class.name.underscore}[#{atrribute_name}_attributes]"
+        lis = object.send(attribute_name).map do |obj|
+          <<-HTML.html_safe
+            <li>
+              <input name="#{scope}[#{object.id}][_destroy]" type="hidden" value="false" />
+              <input name="#{scope}[#{object.id}][asset_path]" type="hidden" value="#{obj.asset_path}" />
+              #{obj.asset_path}
+            </li>
+          HTML
+        end.join("\n").html_safe
+
+        "<div class='file_upload' id='#{genid}'><ul class='asset_list'>#{lis}</ul></div>".html_safe
       end 
       
     end
